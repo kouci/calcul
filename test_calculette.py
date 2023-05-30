f6b1b8b61bbc22f74131124ce8c6410b662e9442
kouci <mansourakociela@gmail.com>
2023-05-30 11:58:19 +0200

    Update test_calculette.py

--- test_calculette.py
+++ test_calculette.py
@@ -7,12 +7,12 @@ def cal():
 
 
 def test_add(cal):
-	cal.add(1,2)
-	assert cal.res == 6
+	cal.add(1,665)
+	assert cal.res == 666
 
 def test_div(cal):
-	cal.divide(1,2)
-	assert cal.res == 0.5
+	cal.divide(1,4)
+	assert cal.res == 0.25
 
 def test_raise(cal):
 	with pytest.raises(Error):
