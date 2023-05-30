e75941086041d9bbd8ef0369826df6e2d0de5955
louis <louis@solidnft.com>
2023-05-30 12:50:44 +0200

    fix for 1ST pr

--- test_calculette.py
+++ test_calculette.py
@@ -8,11 +8,11 @@ def cal():
 
 def test_add(cal):
 	cal.add(10,10)
-	assert cal.res == 18
+	assert cal.res == 20
 
 def test_div(cal):
 	cal.divide(1,10)
-	assert cal.res == 0.8
+	assert cal.res == 0.1
 
 def test_raise(cal):
 	with pytest.raises(Error):
